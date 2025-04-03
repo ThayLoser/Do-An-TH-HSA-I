90541ab7e7bbf5ac836627d91def8462c1ed0018
Truc54 <trungtruc5405@gmail.com>
2025-04-03 19:04:06 +0700

    add file Data_Generator.cpp

---
+++ Data_Generator.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+void GenerateRandomData(int* a, int n)
+{
+	srand(time(NULL));
+
+	for (int i = 0; i < n; i++)
+		a[i] = rand()%n + 0;
+}
+
+void GenerateSortedData(int* a, int n)
+{
+	for (int i = 0; i < n; i++)
+		a[i] = i;
+}
+
+void GenerateReverseData(int* a, int n)
+{
+	for (int i = 0; i < n; i++)
+		a[i] = n - 1 - i;
+}
+
+void GenerateNearlySortedData(int* a, int n)
+{
+    srand(time(NULL));
+	for (int i = 0; i < n; i++)
+		a[i] = i;
+	for (int i = 0; i < n/5; i ++)
+	{
+		int r1 = rand()%n + 0;
+		int r2 = rand()%n + 0;
+		swap(a[r1], a[r2]);
+	}
+}
+
+void GenerateData(int* a, int n, int dataType)
+{
+	switch (dataType)
+	{
+	case 0:	
+		GenerateRandomData(a, n);
+		break;
+	case 1:	
+		GenerateSortedData(a, n);
+		break;
+	case 2:	
+		GenerateReverseData(a, n);
+		break;
+	case 3:	
+		GenerateNearlySortedData(a, n);
+		break;
+	default:
+		printf("unknown data type!\n");
+	}
+}
